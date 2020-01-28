126e91f9f2506c82ae04dcd1b8a2a301763c9b52
Anna-Karenina <>
2020-01-28 14:13:31 +0300

    https add

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,14 @@
 import  axios from 'axios'
 
+const instance = axios.create({
+  httpsAgent: new https.Agent({  
+      rejectUnauthorized: false
+  })
+});
+
 export async function getUsers() {
   try {
-    const response = await axios.get(' https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}');
+    const response = await instance.get(' https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}');
     return response.data
   } catch (error) {
     console.error(error);
