5c890554e070803cd95309173c109929db57f06d
Anna-Karenina <>
2020-01-28 14:20:52 +0300

    https add

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,14 @@
 import  axios from 'axios'
-
-const instance = axios.create({
-  httpsAgent: new https.Agent({  
-      rejectUnauthorized: false
-  })
-});
+import https from 'https'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 export async function getUsers() {
   try {
-    const response = await instance.get(' https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}');
+    const response = await axios.get(' http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}', {
+      httpsAgent: new https.Agent({
+          rejectUnauthorized: false
+      })
+  });
     return response.data
   } catch (error) {
     console.error(error);
